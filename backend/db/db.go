91b20edbe30abd46376dd3edcfc0188dba8a320b
takumu <takumu155695@gmail.com>
2024-12-25 22:15:50 +0900

    .envファイル移動

--- backend/db/db.go
+++ backend/db/db.go
@@ -13,7 +13,7 @@ import (
 func OpenDB() *gorm.DB {
 	// 開発環境の場合に.envファイルを読み込む
 	if os.Getenv("DEV_ENV") == "dev" {
-		err := godotenv.Load("/app/.env")
+		err := godotenv.Load(".env")
 		if err != nil {
 			log.Fatalln(err)
 		}
